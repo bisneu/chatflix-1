5489dc1659b10607f95e260f62d7f3014bf2d3a4
oracle <oracle@bisvieux>
2017-06-15 20:28:14 +0200

    mod

--- recommendation.py
+++ recommendation.py
@@ -74,7 +74,7 @@ class Recommendation:
 
     # Pose une question à l'utilisateur
     def ask_question(self, user):
-        return "Vous aimez les films ?"
+        return "Wesh t'aime les fim ?"
 
     # Calcule la similarité entre 2 utilisateurs
     @staticmethod
